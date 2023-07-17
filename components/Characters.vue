fcb63c86f1eac90aeac0d2c4515047cc00347331
IqroNegoro <iqronegoro0@gmail.com>
2023-07-18 00:03:50 +0700

    fix image ratio

--- components/Characters.vue
+++ components/Characters.vue
@@ -1,7 +1,7 @@
 <template>
     <NuxtLink class="w-48">
-        <div class="w-48 h-auto overflow-hidden">
-            <img :src="character.character.images.webp.image_url" :alt="character.character.name" draggable="false" class="w-48 h-48 object-cover object-top" loading="lazy">
+        <div class="aspect-[4/6] overflow-hidden">
+            <img :src="character.character.images.webp.image_url" :alt="character.character.name" draggable="false" class="object-cover object-top w-full h-full" loading="lazy">
         </div>
         <p class="font-semibold text-lg">{{ character.character.name }}</p>
         <p class="text-sm text-slate-300">{{ character.role }}</p>
--- components/Episodes.vue
+++ components/Episodes.vue
@@ -1,6 +1,6 @@
 <template>
     <NuxtLink :to="`#`" class="w-64 flex flex-col snap-center" draggable="false">
-        <div class="relative group/overlay">
+        <div class="relative group/overlay aspect-video">
             <div class="group/player absolute hidden group-hover/overlay:flex bg-black bg-opacity-50 w-full h-full top-0 left-0 justify-center items-center">
                 <i class="bx bx-play text-7xl transition-all duration-150 opacity-0 translate-y-4 group-hover/player:opacity-100 group-hover/player:translate-y-0"></i>
             </div>
