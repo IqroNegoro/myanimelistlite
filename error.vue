81fe4eeb9df452a71142e78c43fd1715c35eb8fe
IqroNegoro <iqronegoro0@gmail.com>
2023-07-30 16:40:00 +0700

    fix : hydration missmatch on search

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full h-screen bg-black pt-20 flex justify-center items-center flex-col text-white">
+        <Navbar />
+        <img src="./img/kuru.gif" alt="" class="w-48">
+        <p class="font-bold text-3xl">
+            {{ error.statusCode }}
+        </p>
+        <h1>Aduh gk ketemu nih! balik ke home yuk~</h1>
+        <NuxtLink to="/" class="px-2 py-1 rounded-sm bg-red-500 mt-4">
+            Home
+        </NuxtLink>
+    </div>
+</template>
+<script setup>
+defineProps(["error"]);
+</script>
--- pages/search.vue
+++ pages/search.vue
@@ -1,42 +1,47 @@
 <template>
-    <div class="pt-16 px-4" v-if="search">
-        <div class="flex justify-between items-center pr-4 mb-4">
-            <h1 class="text-3xl font-bold">Search for {{q}} {{page ? `Page ${page}` : ''}}</h1>
-            <div class="flex gap-4">
-                <NuxtLink v-if="search.pagination.current_page > 1" :to="{path: '/search', query: {q, page: parseInt(page) - 1}}" class="font-semibold">
-                    <i class="bx bx-chevron-left text-2xl"></i>
-                </NuxtLink>
-                <NuxtLink v-if="search.pagination.has_next_page" :to="{path: '/search', query: {q, page: parseInt(page) + 1}}" class="font-semibold">
-                    <i class="bx bx-chevron-right text-2xl"></i>
+    <ClientOnly>
+        <div class="pt-16 px-4" v-if="search">
+            <div class="flex justify-between items-center pr-4 mb-4">
+                <h1 class="text-3xl font-bold">Search for {{ q }} {{ page ? `&vert; page ${page}` : '' }}</h1>
+                <div class="flex gap-4">
+                    <NuxtLink v-if="search.pagination.current_page > 1"
+                        :to="{ path: '/search', query: { q, page: parseInt(page) - 1 } }" class="font-semibold">
+                        <i class="bx bx-chevron-left text-2xl"></i>
+                    </NuxtLink>
+                    <NuxtLink v-if="search.pagination.has_next_page"
+                        :to="{ path: '/search', query: { q, page: parseInt(page) + 1 } }" class="font-semibold">
+                        <i class="bx bx-chevron-right text-2xl"></i>
+                    </NuxtLink>
+                </div>
+            </div>
+            <div v-if="!q" class="flex justify-center items-center flex-col">
+                <img src="/img/kuru.gif" alt="Loading..." class="w-36">
+                <h1>Gk ketemu nih!</h1>
+            </div>
+            <div v-else-if="search.data.length == 0" class="flex justify-center items-center flex-col">
+                <img src="/img/kuru.gif" alt="Loading..." class="w-36">
+                <h1>Kosong Nih</h1>
+            </div>
+            <div v-else-if="search.data.length" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-flow-row gap-2">
+                <SearchAnime v-for="anime in search.data" :key="anime.mal_id" :anime="anime" />
+            </div>
+            <div v-else-if="error">
+                <img src="/img/kuru.gif" alt="Loading..." class="w-36">
+                <h1>Ada yang salah nih! balik ke rumah yaa~</h1>
+                <NuxtLink to="/" class="px-2 py-1 bg-blue-500">
+                    Home
                 </NuxtLink>
             </div>
+            <div v-else class="flex justify-center items-center">
+                <img src="/img/kuru.gif" alt="Loading...">
+            </div>
         </div>
-        <div v-if="!q" class="flex justify-center items-center flex-col">
-            <img src="/img/kuru.gif" alt="Loading..." class="w-36">
-            <h1>Gk ketemu nih!</h1>
-        </div>
-        <div v-else-if="search.data.length == 0" class="flex justify-center items-center flex-col">
-            <img src="/img/kuru.gif" alt="Loading..." class="w-36">
-            <h1>Kosong Nih</h1>
-        </div>
-        <div v-else-if="search.data.length" class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-flow-row gap-2">
-            <SearchAnime v-for="anime in search.data" :key="anime.mal_id" :anime="anime" />
-        </div>
-        <div v-else-if="error">
-            <img src="/img/kuru.gif" alt="Loading..." class="w-36">
-            <h1>Ada yang salah nih! balik ke rumah yaa~</h1>
-            <NuxtLink to="/" class="px-2 py-1 bg-blue-500">
-                Home
-            </NuxtLink>
-        </div>
-        <div v-else class="flex justify-center items-center">
-            <img src="/img/kuru.gif" alt="Loading...">
-        </div>
-    </div>
+    </ClientOnly>
 </template>
 <script setup>
 const loading = useLoading();
 loading.value = true;
+
 const route = computed(() => useRoute());
 const q = computed(() => route.value.query.q);
 const page = computed(() => route.value.query.page);
@@ -46,6 +51,9 @@ const { data: search, error } = await searchAnime({
         page
     }
 })
+if (!q.value || page.value == 0) {
+    throw createError({ statusCode: 404, message: 'Page Not Found' })
+}
 useHead({
     title: `Search For ${q.value}`
 })
